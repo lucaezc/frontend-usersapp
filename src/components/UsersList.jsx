e7cbdc2a885601871c556919d52147c147ff99e9
Lucas Corbo <corbo.luca@gmail.com>
2024-10-22 19:27:14 -0300

    upload entire project

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import { UserRow } from "./UserRow";
+import { UserContext } from "../context/UserContext";
+import { AuthContext } from "../auth/context/AuthContext";
+
+export const UsersList = () => {
+    
+    const { users } = useContext(UserContext);
+    const { login } = useContext(AuthContext);
+
+    return (
+        <table className="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>username</th>
+                    <th>email</th>
+                    {!login.isAdmin ||  
+                        <>
+                            <th>update</th>
+                            <th>update route</th>
+                            <th>remove</th>
+                        </>
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    users.map(({id, username, email, admin}) => (
+                        <UserRow key={id} id={id} username={username} email={email} admin={admin}/>
+                    ))
+                }
+            </tbody>
+        </table>
+    );
+}
---
+++ src/routes/UserRoutes.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { UsersPage } from "../pages/UsersPage";
+import { Navbar } from "./components/layout/Navbar";
+import { RegisterPage } from "../pages/RegisterPage";
+import { UserProvider } from "../context/UserProvider";
+import { AuthContext } from "../auth/context/AuthContext";
+import { useContext } from "react";
+
+export const UserRoutes = () => {
+    const { login } = useContext(AuthContext);
+
+    return (
+        <>
+            <UserProvider>
+                <Navbar />
+                <Routes>
+                    <Route path="users" element={<UsersPage/>}/>
+                    {!login.isAdmin || 
+                        <>
+                            <Route path="users/register" element={<RegisterPage/>}/>
+                            <Route path="users/edit/:id" element={<RegisterPage/>}/>
+                        </>
+                    }
+                    <Route path="/" element={<Navigate to="/users" />} />
+                </Routes>
+            </UserProvider>
+        </>
+    );
+}
